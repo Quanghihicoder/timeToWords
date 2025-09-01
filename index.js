062327980a6f0ed725982018c995b225e29c53db
Quanghihicoder <hoangquang2508@gmail.com>
2025-09-01 16:11:04 +1000

    solution

---
+++ index.js
@@ -0,0 +1,64 @@
+const NUMBERS = {
+  0: "zero",
+  1: "one", 
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven", 
+  12: "twelve",   
+  13: "thirteen", 
+  14: "fourteen",
+  15: "fifteen",
+  16: "sixteen",
+  17: "seventeen",
+  18: "eighteen",
+  19: "nineteen",
+  20: "twenty",
+  30: "thirty",   
+  40: "forty",   
+  50: "fifty"
+}
+
+function getMinWord(minute) {
+  if (minute === 15) return "quarter"
+  if (minute === 30) return "half"
+
+  if (minute <= 20) {
+    return NUMBERS[minute]
+  } else {
+    const tens = Math.floor(minute / 10) * 10
+    const ones = minute - tens
+
+    if (ones === 0) return NUMBERS[tens]
+    return `${NUMBERS[tens]} ${NUMBERS[ones]}`
+  }
+}
+
+function convertTimeToWords(time) {
+  const [hourStr, minStr] = time.split(":")
+  const hour = parseInt(hourStr, 10)
+  const min = parseInt(minStr, 10)
+
+  const nextHour = hour % 12 + 1
+
+  if (hour === 0 && min === 0) return "midnight"
+  if (hour === 12 && min === 0) return "midday"
+
+  if (min === 0) return `${NUMBERS[hour]} o'clock`
+
+  if (min <= 30) {
+    const minWord = getMinWord(min)
+    return `${minWord} past ${NUMBERS[hour]}`
+  } else {
+    const minWord = getMinWord(60 - min)
+    return `${minWord} to ${NUMBERS[nextHour]}`
+  }
+}
+
+module.exports = { convertTimeToWords }
---
+++ index.test.js
@@ -0,0 +1,33 @@
+const  { convertTimeToWords } = require('./index');
+
+describe('Time to words', () => {
+  it('Handles midnight', () => {
+    const timeInWords = convertTimeToWords('0:00');
+    expect(timeInWords).toBe('midnight');
+  });
+
+  it('Handles 30 - 8:30', () => {
+    const timeInWords = convertTimeToWords('8:30');
+    expect(timeInWords).toBe('half past eight');
+  });
+
+  it('Handles times after 30 mins - 2:45', () => {
+    const timeInWords = convertTimeToWords('2:45');
+    expect(timeInWords).toBe('quarter to three');
+  });
+
+  it('Handles times not 15 not 30 - 2:33', () => {
+    const timeInWords = convertTimeToWords('2:33');
+    expect(timeInWords).toBe('twenty seven to three');
+  });
+
+  it('Handles times not 15 not 30 - 2:11', () => {
+    const timeInWords = convertTimeToWords('2:11');
+    expect(timeInWords).toBe('eleven past two');
+  });
+
+  it('Handles times not 15 not 30 - 12:35', () => {
+    const timeInWords = convertTimeToWords('12:35');
+    expect(timeInWords).toBe('twenty five to one');
+  });
+});
